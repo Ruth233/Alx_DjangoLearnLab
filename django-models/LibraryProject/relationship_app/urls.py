cf91671d8bc81f9dd81b1a946ba43a8054a29e45
Ruth233 <qweba369@gmail.com>
2025-09-07 17:57:00 +0000

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,9 @@ from .views import LibraryDetailView
 from .views import login_view, logout_view, register_view
 from .views import list_books      #required exact line
 from .views import LibraryDetailView   # import class-based view separatel
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -14,6 +17,10 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
     path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+       # Function-based registration view
+    path("register/", views.register_view, name="register"),
+
+    # Built-in class-based login/logout views
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
