45e9a89e1e588312907ad1aba64dc05133740acf
Ruth233 <qweba369@gmail.com>
2025-09-07 18:00:59 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -32,3 +32,35 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("login")  # redirect back to login page
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Helper functions for role checks
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
+
