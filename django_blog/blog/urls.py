f2e2d4324462c2df0663ffb55f962b9e3fc0d9e8
Ruth233 <qweba369@gmail.com>
2025-09-29 15:36:14 +0000

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/templates/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/templates/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
