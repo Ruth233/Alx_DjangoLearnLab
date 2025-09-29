e67a118d32f812ecab35bc59fb097e464c98aa61
Ruth233 <qweba369@gmail.com>
2025-09-29 10:07:40 +0000

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import filters
 
 # Create your views here.
 # 1. List all books
@@ -9,6 +10,12 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]   # public read
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author']
+
+ def perform_create(self, serializer):
+        # Example: attach request.user if your Book model has a 'created_by' field
+        serializer.save()
 
 # 2. Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
