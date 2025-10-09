112ae1df2354f1edea26cf234e0fb07619292be6
Ruth233 <qweba369@gmail.com>
2025-10-09 10:38:30 +0000

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,4 +10,5 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
      path('feed/', FeedView.as_view(), name='user-feed'),
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
