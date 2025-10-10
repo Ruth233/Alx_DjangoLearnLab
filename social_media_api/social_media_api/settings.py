f1dbc0ac5929aab63cbd2a63e8b4bba8100d40d3
Ruth233 <qweba369@gmail.com>
2025-10-10 11:57:35 +0000

    Added PORT to settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'PORT': '3306',
         'default': dj_database_url.config(
          default ='sqlite:///db.sqlite3',
          conn_max_age=600
